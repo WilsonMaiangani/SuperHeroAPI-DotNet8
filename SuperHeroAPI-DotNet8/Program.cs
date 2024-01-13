f41db40f3b6bc65252ffa2a0f653e09ed199d972
WilsonMaiangani <alifattahmaiangani@gmail.com>
2024-01-13 18:26:49 +0100

    Termino

---
+++ SuperHeroAPI-DotNet8/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet8.Data;
+using MySql.Data.MySqlClient;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+void teste()
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    //var connectionString = "Server=localhost;Port=3306;Database=UserManager;Uid=root;Pwd=;";
+    MySqlConnection Conexao = new MySqlConnection(connectionString);
+    Conexao.Open();
+    Conexao.Close();
+}
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    //options.UseMySql(
+    //    connectionString,
+    //    ServerVersion.AutoDetect(connectionString)
+    //    )
+    //.LogTo(Console.WriteLine, LogLevel.Information)
+    //.EnableSensitiveDataLogging()
+    //.EnableDetailedErrors();
+
+    options.UseSqlServer(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+//teste();
+app.Run();
+
